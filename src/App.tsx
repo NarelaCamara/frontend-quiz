94eb07c90aaac34bcffe824660677ff5e32696e0
RoxanaCamara <roxana.camara07@gmail.com>
2025-11-07 16:44:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Welcome to the</h2>
+      <h1>Frontend Quiz!</h1>
+      <p>Pick a subject to get started.</p>
+    </>
+  );
+}
+
+export default App;
