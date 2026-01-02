6f0f9a7295fda8449bd8c3b3d289a47b5da4c95f
RoxanaCamara <roxana.camara07@gmail.com>
2026-01-02 16:14:48 -0300

    feat: corr3ecciones

--- src/App.tsx
+++ src/App.tsx
@@ -16,24 +16,25 @@ function App() {
   return (
     <div>
       <ButtonDarkLightMode />
-      <div className="flex flex-row justify-center gap-4 m-[2%]">
-        <div className="relative -top-5 ">
-          <h2 className="text-[40px] md:text-[64px]">Welcome to the</h2>
-          <h1 className="text-[40px] md:text-[64px] font-semibold">
+      <div className="flex flex-col xl:flex-row items-start xl:items-center xl:justify-center gap-4 m-[2%] xl:gap-12">
+        <div className="relative -top-5 xl:-top-15">
+          <h2 className="text-[40px] md:text-[64px] font-light">
+            Welcome to the
+          </h2>
+          <h1 className="text-[40px] md:text-[64px] font-medium relative -top-5">
             Frontend Quiz!
           </h1>
-          <br className="mb-10 hidden xl:show" />
-          <p className="sm:text-[14px] md:text-[20px] text-[#626C7F] italic my-4">
+
+          <p className="sm:text-[14px] md:text-[20px] text-[#626C7F] italic my-4 relative -top-10">
             Pick a subject to get started.
           </p>
-          <br className="mb-10" />
         </div>
 
-        <div className="flex flex-col gap-4 ">
+        <div className="flex flex-col gap-4 w-full">
           {data.quizzes.map((item: { title: string; icon: string }) => (
             <div
               onClick={() => handleQuizSelection(item.title)}
-              className={`mx-4 w-full min-w-[350px] max-w-[700px]  p-2 xl:p-4 bg-white flex flex-row items-center rounded-xl shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer   hover:bg-gray-100`}
+              className={`w-[85vw] md:w-[60vw]  xl:w-[50%] p-2 xl:p-4 bg-white flex flex-row items-center rounded-xl shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer   hover:bg-gray-100`}
             >
               <Icon icon={item.icon} title={item.title} />
               <p className="text-[#313E51] text-[14px] xl:text-[18px] font-semibold">
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className={`bg-cover bg-pattern `}>
-      <div className="min-w-[460px] md:min-w-auto md:min-h-auto h-full transition-all duration-500 ease-in-out m-[6%] ">
+    <div className={`bg-cover bg-pattern min-w-[320px] `}>
+      <div className="transition-all duration-500 ease-in-out m-[6%] ">
         <Routes />
       </div>
     </div>
