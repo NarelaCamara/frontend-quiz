1eac7f74c2777563deb87b078ac998ce0a97be8e
RoxanaCamara <roxana.camara07@gmail.com>
2026-01-02 15:13:34 -0300

    refactor: correccin bg

--- src/App.tsx
+++ src/App.tsx
@@ -15,34 +15,32 @@ function App() {
 
   return (
     <div>
-      <div className="scale-90 sm:scale-100 min-w-[360px]  sm:p-0 md:p-12 lg:p-24 m-12">
-        <ButtonDarkLightMode />
+      <ButtonDarkLightMode />
+      <div className="flex flex-row justify-center gap-4 m-[2%]">
+        <div className="relative -top-5 ">
+          <h2 className="text-[40px] md:text-[64px]">Welcome to the</h2>
+          <h1 className="text-[40px] md:text-[64px] font-semibold">
+            Frontend Quiz!
+          </h1>
+          <br className="mb-10 hidden xl:show" />
+          <p className="sm:text-[14px] md:text-[20px] text-[#626C7F] italic my-4">
+            Pick a subject to get started.
+          </p>
+          <br className="mb-10" />
+        </div>
 
-        <div className="flex flex-row justify-center gap-4 p-[2%]">
-          <div className="">
-            <h2 className="text-[40px] md:text-[64px]">Welcome to the</h2>
-            <h1 className="text-[40px] md:text-[64px] font-semibold">
-              Frontend Quiz!
-            </h1>
-            <br className="mb-10 hidden xl:show" />
-            <p className="sm:text-[14px] md:text-[20px] text-[#626C7F] italic my-4">
-              Pick a subject to get started.
-            </p>
-            <br className="mb-10" />
-          </div>
-          <div className="flex flex-col gap-4 ">
-            {data.quizzes.map((item: { title: string; icon: string }) => (
-              <div
-                onClick={() => handleQuizSelection(item.title)}
-                className={`mx-4 w-full min-w-[350px] max-w-[700px]  p-2 xl:p-4 bg-white flex flex-row items-center rounded-xl shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer   hover:bg-gray-100`}
-              >
-                <Icon icon={item.icon} title={item.title} />
-                <p className="text-[#313E51] text-[14px] xl:text-[18px] font-semibold">
-                  {item.title}
-                </p>
-              </div>
-            ))}
-          </div>
+        <div className="flex flex-col gap-4 ">
+          {data.quizzes.map((item: { title: string; icon: string }) => (
+            <div
+              onClick={() => handleQuizSelection(item.title)}
+              className={`mx-4 w-full min-w-[350px] max-w-[700px]  p-2 xl:p-4 bg-white flex flex-row items-center rounded-xl shadow-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer   hover:bg-gray-100`}
+            >
+              <Icon icon={item.icon} title={item.title} />
+              <p className="text-[#313E51] text-[14px] xl:text-[18px] font-semibold">
+                {item.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className={`bg-pattern bg-no-repeat `}>
-      <div className="min-w-[460px] md:min-w-auto md:min-h-auto transition-all duration-500 ease-in-out ">
+    <div className={`bg-cover bg-pattern `}>
+      <div className="min-w-[460px] md:min-w-auto md:min-h-auto h-full transition-all duration-500 ease-in-out m-[6%] ">
         <Routes />
       </div>
     </div>
