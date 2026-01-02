0125a50ba950a464c7054f685df9415bafc9686e
RoxanaCamara <roxana.camara07@gmail.com>
2026-01-02 14:54:35 -0300

    feat: correccion icon

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -4,5 +4,11 @@ export const Icon = ({ icon, title }: { icon: string; title: string }) => {
   const bg =
     colorIcon[title.toLocaleLowerCase() as keyof typeof colorIcon] ||
     "bg-gray-200";
-  return <img src={icon} alt="icon" className={`p-2 mr-2 ${bg} rounded-xl `} />;
+  return (
+    <img
+      src={icon}
+      alt="icon"
+      className={`w-10 h-10 p-2 mr-2 ${bg} rounded-xl `}
+    />
+  );
 };
